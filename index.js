840ff24f3053cd2e509663bac5ca00be2999debf
HssngH <97723124+HssngH@users.noreply.github.com>
2022-01-17 23:20:27 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+        var lastage = 90-age;
+        var days = lastage*365;
+        var weeks = lastage*52;
+        var months = lastage*12;
+        console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+        
+        
+        
+        
+        
+    /*************Don't change the code below**********/
+    }
+    
+
+
