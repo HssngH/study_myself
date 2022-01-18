d52b04b4294f353a1b9e09a49cb298dabbc3ed95
HssngH <97723124+HssngH@users.noreply.github.com>
2022-01-18 13:24:31 +0900

    Add files via upload

---
+++ [js]practice.js
@@ -0,0 +1,43 @@
+// bmi Calculator
+
+/*
+function bmiCalculator (weight, height) {
+    var interpretation = weight/(height*height);
+    return interpretation;
+}
+
+if (interpretation < 18.5) {
+    console.log("Your BMI is " + bmiCalculator + ", so you are underweight.");
+}
+
+if (interpretation >= 18.5 && interpretation <=24.9) {
+    console.log("Your BMI is " + bmiCalculator + ", so you have a normal weight.");
+}
+
+if (interpretation > 24.9) {
+    console.log("Your BMI is " + bmiCalculator + ", so you are overweight.");
+}
+
+bmiCalculator(50,1.8);
+*/
+
+//leap year
+
+/*
+function isLeap(year) {
+  if (year%4===0) {
+    if (year%100===0) {
+      if (year%400===0) {
+        return "Leap year."
+      } else {
+        return "Not leap year."
+      }
+    } else {
+      return "Leap year."
+    }
+  } else {
+    return "Not leap year."
+  }
+}
+
+*/
